d899a419ce38ee632d0acf2e02ddff2ebebf38ea
Eugene <zkhakhunov@mail.ru>
2024-02-26 22:25:18 +0300

    Первое задание полностью готово

--- 2.functions/task.js
+++ 2.functions/task.js
@@ -1,4 +1,17 @@
 function getArrayParams(...arr) {
+  let min = -Infinity;
+  let max = Infinity;
+  let sum = 0;
+    for (let i = 0; i < arr.length; i++){
+      if (arr[i] > arr[i-1]){
+        max = arr[i];
+      }
+      if (arr[i] < arr[i+1]){
+        min =  arr[i];
+        }
+    sum += arr[i];
+    }
+    avg = Number((sum/arr.length).toFixed(2));
 
   return { min: min, max: max, avg: avg };
 }
