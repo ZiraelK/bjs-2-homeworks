4ebf06ab7b5feec4c252c2ea56c3f55b6e00583c
Eugene <zkhakhunov@mail.ru>
2024-02-28 00:30:46 +0300

    Первое задание полностью готово

--- 2.functions/task.js
+++ 2.functions/task.js
@@ -48,7 +48,16 @@ function differenceEvenOddWorker(...arr) {
 }
 
 function averageEvenElementsWorker(...arr) {
-
+  let sumEvenElement = 0;
+  let countEvenElement = 0;
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] % 2 === 0) {
+         sumEvenElement += arr[i];
+         countEvenElement += 1;
+    } 
+  }
+  let avrEven =  sumEvenElement/countEvenElement;
+  return avrEven;
 }
 
 function makeWork (arrOfArr, func) {
