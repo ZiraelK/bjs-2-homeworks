9d7393ca18e7d907cf9c7cbbd3ef9fa71ee8af86
Eugernation <zkhakhunov@mail.ru>
2024-03-22 19:24:45 +0300

    Initial commit

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,19 +1,40 @@
 function Student(name, gender, age) {
-  
+    this.name = name;
+    this.gender = gender;
+    this.age = age;
+    this.marks = [];
 }
 
+let student1 = new Student("Василиса", "женский", 19);
+let student2 = new Student("Артём", "мужской", 25);
+
 Student.prototype.setSubject = function (subjectName) {
-  
+    this.subject = subjectName;
 }
 
 Student.prototype.addMarks = function (...marks) {
-  
+    if (this.marks.length == 0) {
+        return;
+      } else {
+        this.marks = marksToAdd;
+      }
 }
 
 Student.prototype.getAverage = function () {
-  
+    let sum = 0;
+    let length = this.marks.length;
+    if (length == 0) {
+      return 0;
+    } else {
+      for (let i = 0; i < length; i += 1) { 
+        sum += numbers[i]; 
+      }
+      return sum / length;
+    }
 }
 
 Student.prototype.exclude = function (reason) {
-  
+    delete this.subject;
+    delete this.marks;
+    this.excluded = reason;
 }
